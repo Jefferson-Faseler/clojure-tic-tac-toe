c89b9576e094bd12211b6f794fc6e66bfa3382a4
Jefferson-Faseler <jjfaseler@gmail.com>
2017-03-28 10:33:08 -0400

    Passing tests for finding if a move is valid.

--- spec/ttt_with_yossi/core_spec.clj
+++ spec/ttt_with_yossi/core_spec.clj
@@ -9,6 +9,22 @@
   (it "as a vector"
     (should= [0 1 2 3 4 5 6 7 8] new-board)))
 
+
+
+(describe "Places a symbol on the board"
+
+          (defn can-place? [board square]
+            (number? (get board square)))
+
+  (describe "should check if the square is available"
+    (it "returns true if the symbol can be played"
+      (should= true (can-place? [0 1 2 3 4 5 :X 7 8] 5)))
+    (it "returns false is the space is occupied"
+      (should= false (can-place? [0 1 2 3 4 5 :X 7 8] 6))))
+
+
+(describe "grab positions"
+
           (defn grab-positions [board]
             (concat (partition-all 3 board)
             (list
@@ -16,9 +32,8 @@
               (take-nth 3 (drop 1 board)) ;(1 4 7)
               (take-nth 3 (drop 2 board)) ;(2 5 8)
               (take-nth 4 board);(0 4 8)
-              (take 3 (take-nth 2 (drop 2 board))))))
+              (take 3 (take-nth 2 (drop 2 board)))))))
 
-(describe "grab positions"
   (it "creates a list of possible winning positions"
     (should= '((0 1 2) (3 4 5) (6 7 8) (0 3 6) (1 4 7) (2 5 8) (0 4 8) (2 4 6))
      (grab-positions new-board))))
