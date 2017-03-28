f698a0ea5291dcb8321b5e1469b8eae8469316fb
Jefferson-Faseler <jjfaseler@gmail.com>
2017-03-28 10:11:14 -0400

    Refactored passing tests for board creation and grabbing positions/

--- spec/ttt_with_yossi/core_spec.clj
+++ spec/ttt_with_yossi/core_spec.clj
@@ -3,26 +3,22 @@
             [ttt-with-yossi.core :refer :all]))
 
 (describe "Makes a board"
+
+          (def new-board (range 0 9))
+
   (it "as a vector"
-    (should= [0 1 2 3 4 5 6 7 8] (range 0 9))))
+    (should= [0 1 2 3 4 5 6 7 8] new-board)))
 
-(def board (range 0 9))
-
-(defn grab-positions [board]
-  (concat (partition-all 3 board)
-  (list
-    (take-nth 3 board) ; (0 3 6)
-    (take-nth 3 (drop 1 board)) ;(1 4 7)
-    (take-nth 3 (drop 2 board)) ;(2 5 8)
-    (take-nth 4 board);(0 4 8)
-    (take 3 (take-nth 2 (drop 2 board))))
-  )
-)
-
+          (defn grab-positions [board]
+            (concat (partition-all 3 board)
+            (list
+              (take-nth 3 board) ; (0 3 6)
+              (take-nth 3 (drop 1 board)) ;(1 4 7)
+              (take-nth 3 (drop 2 board)) ;(2 5 8)
+              (take-nth 4 board);(0 4 8)
+              (take 3 (take-nth 2 (drop 2 board))))))
 
 (describe "grab positions"
   (it "creates a list of possible winning positions"
     (should= '((0 1 2) (3 4 5) (6 7 8) (0 3 6) (1 4 7) (2 5 8) (0 4 8) (2 4 6))
-     (grab-positiong s board))
-  )
-)
+     (grab-positions new-board))))
