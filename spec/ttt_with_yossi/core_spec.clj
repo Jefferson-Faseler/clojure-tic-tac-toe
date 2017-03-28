f3ccf491ef74e833216e12d666d581b095981c42
Jefferson-Faseler <jjfaseler@gmail.com>
2017-03-28 13:59:09 -0400

    Refactor negamax and score function

--- spec/ttt_with_yossi/core_spec.clj
+++ spec/ttt_with_yossi/core_spec.clj
@@ -102,39 +102,40 @@
                                       :X :O  5
                                        6  7 :X])))
 
-            (defn placement-score [board index depth]
+            (defn complete-board-score [board index depth]
               (cond
                 (win? board) {:score (- 10 depth) :index index}
                 (full? board) {:score 0 :index index}))
 
   (it "returns a score based on depth and returns an index for won board"
     (should= {:score 9 :index 6}
-            (placement-score [:X :O  2
-                              :X :O  5
-                              :X  7 :X] 6 1)))
+            (complete-board-score [:X :O  2
+                                   :X :O  5
+                                   :X  7 :X] 6 1)))
 
   (it "returns a score of 0 and index for draw game"
-    (should= {:score 0 :index 8} (placement-score [:X :O :O
-                                                   :O :X :X
-                                                   :X :O :O] 8 3)))
+    (should= {:score 0 :index 8} (complete-board-score [:X :O :O
+                                                        :O :X :X
+                                                        :X :O :O] 8 3)))
 
   (it "returns nil if board is not won or full"
-    (should= nil (placement-score new-board 0 0)))
+    (should= nil (complete-board-score new-board 0 0)))
 
             (defn negamax [board symbol index depth]
-              (if-let [placement-score (placement-score board index depth)]
+              (if-let [placement-score (complete-board-score board index depth)]
                 placement-score))
 
   (describe "negamax"
     (describe "returns palcement score if board is complete"
       (it "win/loss board"
-        (should= (placement-score [:X :O 2 :X :O 5 :X 7 :X] 6 1) (negamax [:X :O 2 :X :O 5 :X 7 :X] :O 6 1)))
+        (should= (complete-board-score [:X :O 2 :X :O 5 :X 7 :X] 6 1) (negamax [:X :O 2 :X :O 5 :X 7 :X] :O 6 1)))
 
       (it "full board"
-        (should= (placement-score [:X :O :O :O :X :X :X :O :O] 8 3) (negamax [:X :O :O :O :X :X :X :O :O] :X 8 3)))))
+        (should= (complete-board-score [:X :O :O :O :X :X :X :O :O] 8 3) (negamax [:X :O :O :O :X :X :X :O :O] :X 8 3))))
 
-    (it "returns nil if board is not complete"
-      (should= (placement-score new-board 0 0) (negamax new-board :X 0 0)))
+    (describe "if board is not complete"
+      (it "returns nil"
+        (should= (complete-board-score new-board 0 0) (negamax new-board :X 0 0)))))
 
   ) ;; negamax
 
